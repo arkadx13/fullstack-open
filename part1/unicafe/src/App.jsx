40089f1b9a41546e0c9563e9d89190806066d47e
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2024-08-02 17:33:00 +0800

    unicafe 1.7 done; renamed FeedbackTally to FeedbackStatistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,16 +4,25 @@ const FeedbackButton = ({ onClick, text }) => (
 	<button onClick={onClick}>{text}</button>
 );
 
-const FeedbackTally = ({ value, text }) => (
-	<div>
-		{text} {value}
-	</div>
-);
+const FeedbackStatistics = ({ value, text }) => {
+	console.log(text, value);
+
+	return (
+		<div>
+			{text} {value}
+		</div>
+	);
+};
 
 const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const feedbackCounts = good + neutral + bad;
+	// Scoring => good: 1, neutral: 0, bad: -1
+	const totalScore = good - bad;
+	const averageScore = totalScore / feedbackCounts;
+	const positivePercentage = (good / feedbackCounts) * 100;
 
 	const handleGoodFeedback = () => setGood(good + 1);
 	const handleNeutralFeedback = () => setNeutral(neutral + 1);
@@ -27,9 +36,15 @@ const App = () => {
 			<FeedbackButton onClick={handleBadFeedback} text="bad" />
 
 			<h1>statistics</h1>
-			<FeedbackTally value={good} text="good" />
-			<FeedbackTally value={neutral} text="neutral" />
-			<FeedbackTally value={bad} text="bad" />
+			<FeedbackStatistics value={good} text="good" />
+			<FeedbackStatistics value={neutral} text="neutral" />
+			<FeedbackStatistics value={bad} text="bad" />
+			<FeedbackStatistics value={feedbackCounts} text="all" />
+			<FeedbackStatistics value={averageScore || 0} text="average" />
+			<FeedbackStatistics
+				value={positivePercentage || 0}
+				text="positive"
+			/>
 		</div>
 	);
 };
