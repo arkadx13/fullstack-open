124edee63cf68ee8f7f1a890565454f330cf48a0
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2024-08-04 14:41:29 +0800

    phonebook 2.8 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
 const Contact = ({ person }) => {
-	return <p>{person.name}</p>;
+	return (
+		<p>
+			{person.name} {person.number}
+		</p>
+	);
 };
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "040-1234567" },
+	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const handleAddContact = (event) => {
 		event.preventDefault();
@@ -19,12 +26,17 @@ const App = () => {
 			return;
 		}
 
-		const newPerson = {
-			name: newName,
-		};
+		// make sure all fields submitted are filled
+		if (newName !== "" && newNumber !== "") {
+			const newPerson = {
+				name: newName,
+				number: newNumber,
+			};
 
-		setPersons(persons.concat(newPerson));
-		setNewName("");
+			setPersons(persons.concat(newPerson));
+			setNewName("");
+			setNewNumber("");
+		}
 	};
 
 	return (
@@ -35,7 +47,14 @@ const App = () => {
 					name:{" "}
 					<input
 						value={newName}
-						onChange={() => setNewName(event.target.value)}
+						onChange={(event) => setNewName(event.target.value)}
+					/>
+				</div>
+				<div>
+					number:{" "}
+					<input
+						value={newNumber}
+						onChange={(event) => setNewNumber(event.target.value)}
 					/>
 				</div>
 				<div>
