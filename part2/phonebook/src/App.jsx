af52c3082b57ff02778c0a12375b02f9b166db40
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2024-08-06 15:43:00 +0800

    phonebook 2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,28 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-
-const Contact = ({ person }) => {
-	return (
-		<p>
-			{person.name} {person.number}
-		</p>
-	);
-};
+import axios from "axios";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [keyword, setKeyword] = useState("");
 	const [filteredPersons, setFilteredPersons] = useState([]);
 	const personsToShow = keyword === "" ? persons : filteredPersons;
+
+	useEffect(() => {
+		axios.get("http://localhost:3001/persons").then((response) => {
+			setPersons(response.data);
+		});
+	}, []);
 
 	const handleAddContact = (event) => {
 		event.preventDefault();
@@ -40,7 +34,7 @@ const App = () => {
 			const newPerson = {
 				name: newName.trim(),
 				number: newNumber.trim(),
-				id: persons.length + 1,
+				id: String(persons.length + 1),
 			};
 
 			setPersons(persons.concat(newPerson));
